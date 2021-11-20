540fd7ba29a76677b18f48ac06fead2b6441468e
telman03 <telmangadimov1@gmail.com>
2021-11-20 15:10:11 +0400

    adding about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', views.homePageView, name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
+from django import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
